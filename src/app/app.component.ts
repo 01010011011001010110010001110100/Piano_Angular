cee8d970e6a28def6aed1349559f50d715d238e3
Ronel Pantaleon <r.pantaleon@castilloss.com>
2024-12-09 10:52:55 -0400

    Add the Guide items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PianoComponent } from './components/piano/piano.component';
+import { ButtonComponent } from './components/buttons/button/button.component';
+import { ItemSelectionComponent } from './components/buttons/item-selection/item-selection.component';
+import { PianoKeyComponent } from './components/buttons/piano-key/piano-key.component';
+import { EkeyType } from './components/enums/EkeyType';
+import { KeyNoteComponent } from './components/key-note/key-note.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    PianoComponent
+    PianoComponent,
+    ButtonComponent,
+    ItemSelectionComponent,
+    PianoKeyComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // Enum keys
+  public blackType: EkeyType;
+  public whiteType: EkeyType;
+
   title = 'Piano_Angular';
+
+  constructor() {
+    this.blackType = EkeyType.BLACK;
+    this.whiteType = EkeyType.WHITE;
+  }
 }
